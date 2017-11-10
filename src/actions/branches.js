3626e7298c3c108730df149f12d3b4d34b047e82
romain.lenzotti <rom.lenzotti@gmail.com>
2017-11-10 10:12:39 +0100

    feat: add switch options

--- src/actions/branches.js
+++ src/actions/branches.js
@@ -1,44 +1,142 @@
 "use strict";
 const chalk = require("chalk");
 const figures = require("figures");
-const {branchesInfos, currentBranchName} = require("./git");
+const inquirer = require("inquirer");
+
+const {branchesInfos, currentBranchName, checkout} = require("./git");
 const DEFAULT_OPTIONS = {
   master: "master",
   production: "production"
 };
+const BRANCHES = new Map();
 
-module.exports = (options = DEFAULT_OPTIONS) => {
-
+/**
+ *
+ * @param options
+ */
+function runInteractive(options = DEFAULT_OPTIONS) {
   options = Object.assign({}, DEFAULT_OPTIONS, options);
+  const choices = buildBranchesList(options)
+    .map((info) => {
+      return {value: info.branch, name: info.message, short: info.branch};
+    });
 
+  inquirer.prompt([
+    {
+      "type": "list",
+      "name": "branchChoice",
+      "message": "Select branch to switch on",
+      "choices": choices.concat([new inquirer.Separator(), "exit"])
+    }
+  ])
+    .then((answers) => {
+      if (answers.branchChoice !== "exit") {
+        switchBranch(answers.branchChoice, options);
+      }
+    }).catch(er => console.error(er));
+}
+
+/**
+ *
+ * @param branch
+ * @param options
+ */
+function switchBranch(branch, options) {
+  let branchInfo = BRANCHES.get(branch);
+  let observable;
+
+  if (branchInfo) {
+    observable = checkout(branchInfo.local ? branchInfo.local.branch : branchInfo.branch);
+  } else {
+    const branchName = branch.split("/")[1];
+    branchInfo = BRANCHES.get(branchName);
+    if (branchInfo.local) {
+      observable = checkout(branchInfo.local.branch);
+    } else {
+      observable = checkout("-b", branchName, branchInfo.branch);
+    }
+  }
+
+  observable.subscribe((o) => {
+    console.log(o);
+  });
+
+  return observable;
+}
+
+/**
+ *
+ * @returns {Array.<*>}
+ */
+function branches() {
+
+  const remoteBranches = branchesInfos("-r");
+  const localBranches = branchesInfos();
+
+  remoteBranches.forEach((branchInfo) => {
+    const branch = branchInfo.branch.split("/")[1];
+    BRANCHES.set(branch, branchInfo);
+  });
+
+  localBranches.forEach((branchInfo) => {
+    const branch = branchInfo.branch;
+    if (!BRANCHES.has(branch)) {
+      BRANCHES.set(branch, branchInfo);
+    } else {
+      BRANCHES.get(branch).local = branchInfo;
+    }
+  });
+
+  const list = [];
+  BRANCHES.forEach(branch => {
+    if (branch.local) {
+      if (branch.date !== branch.local.date) {
+        list.push(branch.local);
+      }
+    }
+    list.push(branch);
+  });
+  return list.sort((info1, info2) => info1.date < info2.date);
+}
+
+/**
+ *
+ * @param options
+ * @returns {*}
+ */
+function buildBranchesList(options) {
   let isUnderProduction = false;
   const currentBranch = currentBranchName();
-  return branchesInfos().forEach((info) => {
-    const branch = info.branch.split("/")[1];
-    const line = info.date + " " + column(info.creation, 15) + " " + " " + column(info.author, 25) + " " + info.branch;
-    let current = " ";
-    if (currentBranch === branch) {
-      current = chalk.yellow(figures.star);
-    }
 
-    if (branch === options.production) {
-      console.log(current + " " + chalk.yellow(figures.warning) + " ", chalk.yellow(line));
-      isUnderProduction = true;
-      return;
-    }
-    if (branch === "origin/" + options.master) {
-      console.log(current + " " + chalk.gray(figures.bullet) + " ", chalk.gray(line));
-      return;
-    }
+  return branches()
+    .map((info) => {
+      const branch = info.branch.split("/")[1];
+      const line = info.date + " " + column(info.creation, 15) + " " + " " + column(info.author, 20) + " " + info.branch;
+      let current = " ";
+      if (currentBranch === branch) {
+        current = chalk.yellow(figures.star);
+      }
 
-    if (!isUnderProduction) {
-      console.log(current + " " + chalk.green(figures.tick) + " ", line);
-    } else {
-      console.log(current + " " + chalk.red(figures.cross) + " ", chalk.red(line));
-    }
+      if (branch === options.production) {
+        isUnderProduction = true;
+        info.message = current + " " + chalk.yellow(figures.warning) + " " + chalk.yellow(line);
+        return info;
+      }
 
-  });
-};
+      if (branch === "origin/" + options.master) {
+        info.message = current + " " + chalk.gray(figures.bullet) + " " + chalk.gray(line);
+        return info;
+      }
+
+      if (!isUnderProduction) {
+        info.message = current + " " + chalk.green(figures.tick) + " " + line;
+        return info;
+      }
+
+      info.message = current + " " + chalk.red(figures.cross) + " " + chalk.red(line);
+      return info;
+    });
+}
 
 function line(str = "", length = 100, char = "-") {
   let finalStr = str;
@@ -51,3 +149,5 @@ function line(str = "", length = 100, char = "-") {
 function column(str = "", length = 30, char = " ") {
   return line(str, length, char);
 }
+
+module.exports = runInteractive;
