21d674e2b20529fe25ae86a2d72722ad6c8c2aaf
romain.lenzotti <rom.lenzotti@gmail.com>
2018-08-24 14:25:26 +0200

    fix: Update configuration
    (cherry picked from commit bbb9c4abeebe29c31d411e5246b15b5eb3387c9a)

--- release.config.js
+++ release.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  branch: 'master',
+  branch: 'production',
   prepare: ['@semantic-release/npm', './src/command/release.js'],
   success: ['@semantic-release/github', './src/command/release.js'], // ,
   fail: ['@semantic-release/github'],
-  npmPublish: false
 };
